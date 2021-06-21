37ee01682a5a7f80badbcb788a9be9ab3167d6a0
Sebastian San Blas <sebastiansanblas@gmail.com>
2021-06-20 21:18:30 -0300

    Initial Commit

---
+++ PassGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassGenerator
+//
+//  Created by Sebastian San Blas on 20/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
